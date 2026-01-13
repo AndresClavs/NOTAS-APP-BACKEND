414555facce5065f54ad39ca2f0e8a1bfbc33471
Juan Andrés Clavón <aclavon17@gmail.com>
2026-01-12 21:48:03 -0500

    Agregamos a cors el origen de nuestro front

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
 
+dotenv.config();
+
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost5173",  "https://heartfelt-bonbon-258ac2.netlify.app/" ],
+}
+
+));
 app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
